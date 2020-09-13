195baaaa9e4af2e729937d52711b9cb640240ae1
ojgup <102542177@Student.swin.edu.au>
2020-09-13 10:01:09 +1000

    Added the questions and cleaned up code.

--- NHRM-APP/src/app/components/qol/qol.component.ts
+++ NHRM-APP/src/app/components/qol/qol.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormArray, FormGroup} from '@angular/forms';
 import {FormlyFieldConfig, FormlyFormOptions} from '@ngx-formly/core';
 
 @Component({
@@ -9,34 +9,70 @@ import {FormlyFieldConfig, FormlyFormOptions} from '@ngx-formly/core';
 })
 export class QolComponent implements OnInit {
 
-  form = new FormGroup({});
-  model: any = {
-    questions: ["I have no problems in walking about", "I have slight problems in walking about", 
-    "I have moderate problems in walking about", "I have severe problems in walking about", "I am unable to walk about"],
-    score: 0
-  };
-  options: FormlyFormOptions = {};
 
-  fields: FormlyFieldConfig[] = [ {
-    key: 'score',
-    type: 'radio',
-    templateOptions: {
-      required: true,
-      label: "Mobility",
-      options: [
-          { value: 1, label: this.model.questions[0] },
-          { value: 2, label: this.model.questions[1] },
-          { value: 3, label: this.model.questions[2] },
-          { value: 4, label: this.model.questions[3] },
-          { value: 5, label: this.model.questions[4] },
-        ],
-      }
-    }, 
+
+  model: any = {};
+  
+  // The survey categories and questions retrieved from the API
+  survey: any = [{
+      categoryName: "Mobility",
+      questions: ["I have no problems in walking about", "I have slight problems in walking about", 
+      "I have moderate problems in walking about", "I have severe problems in walking about", "I am unable to walk about"],
+    },
+    {
+      categoryName: "Self Care",
+      questions: ["I have no problems washing or dressing myself", "I have slight problems washing or dressing myself", 
+      "I have moderate problems washing or dressing myself", "I have severe problem washing or dressing myself",
+      "I am unable to wash or dress myself"],
+    },
+    {
+      categoryName: "Usual Activites",
+      questions: ["I have no problems doing my casual activities", "I have slight problems doing my casual activites",
+    "I have moderate problems doing my usual activities", "I have severe problems doing my usual activites",
+    "I am unable to do my usual activities"]
+    },
+    {
+      categoryName: "Pain/Discomfort",
+      questions: ["I have no pain or discomfort", "I have slight pain or discomfort", 
+      "I have moderate pain or discomfort", "I have severe pain or discomfort", 
+      "I have extreme pain or discomfort"]
+    }
   ];
 
-  constructor() { }
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[] 
+  form = new FormGroup({});
+  currentCategory: number;
+
+  displayQuestions() {
+    this.fields = [ {
+      key: this.survey[this.currentCategory].categoryName,
+      type: 'radio',
+      templateOptions: {
+        label: this.survey[this.currentCategory].categoryName,
+        options: [
+            { value: 1, label: this.survey[this.currentCategory].questions[0] },
+            { value: 2, label: this.survey[this.currentCategory].questions[1] },
+            { value: 3, label: this.survey[this.currentCategory].questions[2] },
+            { value: 4, label: this.survey[this.currentCategory].questions[3] },
+            { value: 5, label: this.survey[this.currentCategory].questions[4] },
+          ],
+          required: true
+        }
+      }, 
+    ];
+  }
 
   ngOnInit(): void {
+    this.currentCategory = 0;
+    this.displayQuestions();
+  }
+
+  submit() {
+    alert(JSON.stringify(this.model));
+
+    this.currentCategory += 1;
+    this.displayQuestions();
   }
 
 }
