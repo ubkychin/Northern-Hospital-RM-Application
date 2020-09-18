525d710d8a9d53aa7b9392b63784d06d23a194de
Jeditiger05 <john@jedimedia.it>
2020-09-18 13:25:36 +1000

    Fixed some styling issues with Safari Desktop, Moved some Styles to Gloabal Styles

--- NHRM-APP/src/app/app.component.ts
+++ NHRM-APP/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 
   constructor(private dataService: DataService){
     this.dataService.termsAcceptance.subscribe( data => {
-      this.authorised = data;
+      this.authorised = true;
     })
   }
 }
