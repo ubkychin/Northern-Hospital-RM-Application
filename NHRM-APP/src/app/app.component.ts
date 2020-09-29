315682c63972d97298a86ab57aaf1090e0704f6c
Jeditiger05 <john@jedimedia.it>
2020-09-29 19:32:02 +1000

    Fixed Nav Hamburger to work with IOS using JS

--- NHRM-APP/src/app/app.component.ts
+++ NHRM-APP/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import {DataService} from './services/data.service';
-import {Router} from '@angular/router';
-import {Location} from '@angular/common';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { DataService } from './services/data.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +11,45 @@ import {Location} from '@angular/common';
 export class AppComponent {
   title = 'NHRM-APP';
 
+  @ViewChild('bars') toggleNav: ElementRef;
+  toggle: boolean = true;
   authorised: boolean;
   isRoot: boolean;
 
-  constructor(private dataService: DataService, private router: Router, private location: Location){
-    this.dataService.termsAcceptance.subscribe( data => {
+  constructor(private dataService: DataService, private router: Router,
+    private location: Location, private renderer: Renderer2) {
+    this.dataService.termsAcceptance.subscribe(data => {
       this.authorised = /* data; */true;
     })
 
     this.router.events.subscribe(event => {
-      if(this.location.path() !== '/home')
+      if (this.location.path() !== '/home')
         this.isRoot = false;
       else
         this.isRoot = true;
     })
+
+    //Dropdown Nav
+    this.renderer.listen('window', 'click', (e: Event) => {
+      console.log("Clicked")
+
+      console.log(e.target)
+      console.log(this.toggleNav.nativeElement)
+
+      if (e.target !== this.toggleNav.nativeElement) {
+        this.toggle = true
+      } else if (this.toggle == true) {
+        this.toggle = false
+      }
+    })
+
   }
 
-  goBack(){
+  goBack() {
     this.location.back();
+  }
+
+  dropdown() {
+
   }
 }
