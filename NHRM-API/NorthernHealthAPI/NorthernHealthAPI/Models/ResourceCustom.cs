fb266a115a6eeb7d4e62195d4ebb29da9b770fab
Steve G <s.grouios@hotmail.com>
2020-10-10 15:20:05 +1100

    Added custom Resource model

---
+++ NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Models/ResourceCustom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthernHealthAPI.Models
+{
+    public class ResourceCustom
+    {
+        public int ResourceID { get; set; }
+        public string Title { get; set; }
+        public string Prompt { get; set; }
+        public int CategoryID { get; set; }
+        public dynamic Content { get; set; }
+        public int TypeID { get; set; }
+    }
+}
