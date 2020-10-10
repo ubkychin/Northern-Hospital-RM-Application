04c718e28ffbb88dc07bac819a13d709db2ab02a
Jeditiger05 <john@jedimedia.it>
2020-10-11 03:26:42 +1100

    Moved Surveys to IPC Screen. Made My IPC Survey Buttons Dynamic

--- NHRM-APP/src/app/components/survey-nav/survey-nav.component.ts
+++ NHRM-APP/src/app/components/survey-nav/survey-nav.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveyNavComponent implements OnInit {
 
-  constructor() { }
+  surveys: string [];
+
+  constructor() {
+    this.surveys = ["Survey ", "Survey "] 
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
