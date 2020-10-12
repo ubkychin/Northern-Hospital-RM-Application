e024e3885c54c9acce86a304a1afa476392b6ee9
Steve G <s.grouios@hotmail.com>
2020-10-12 16:41:13 +1100

    Added ResourceFilter pipe and implemented in Patient Resources

---
+++ NHRM-APP/src/app/pipes/resource-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ResourceFilterPipe } from './resource-filter.pipe';
+
+describe('ResourceFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ResourceFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
