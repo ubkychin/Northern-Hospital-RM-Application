1e97212fcfc9ccdc0178aef84ba6a88ab547dacf
ojgup <102542177@Student.swin.edu.au>
2020-10-19 10:55:58 +1100

    Added the questions for the HADS.

--- NHRM-APP/src/app/services/data.service.ts
+++ NHRM-APP/src/app/services/data.service.ts
@@ -125,22 +125,144 @@ export class DataService {
     let list: any = [
       {
         categoryName: "Anxiety",
-        prompt: "I feel tense or 'wound up': ",
-        options: ["Most of the time", "A lot of time", "From time to time, occasionally", "Not at all"] // 3, 2, 1, 0
+        prompt: "I feel tense or 'wound up':",
+        list: [
+          {option: "Most of the time", value: 3}, 
+          {option: "A lot of time", value: 2}, 
+          {option: "From time to time, occasionally", value: 1}, 
+          {option: "Not at all", value: 0}
+        ] // 3, 2, 1, 0
       },
       {
         categoryName: "Depression",
         prompt: "I still enjoy the things I used to enjoy:",
-        options: ["Definitely as much", "Not quite so much", "Only a little", "Hardly at all"] // 0, 1, 2, 3
+        list: [
+          {option: "Definitely as much", value: 0}, 
+          {option: "Not quite so much", value: 1}, 
+          {option: "Only a little", value: 2}, 
+          {option:"Hardly at all", value: 3}
+        ] // 0, 1, 2, 3
       },
       {
         categoryName: "Anxiety",
-        prompt: "I get a sort of frightened feeling as if something awful is about to happen",
-        options: ["Very definitely and quite badly ", "Yes, but not too badly ", "A little, but it doesn't worry me", "Not at all"] // 3, 2, 1, 0
+        prompt: "I get a sort of frightened feeling as if something awful is about to happen:",
+        list: [
+          {option: "Very definitely and quite badly", value: 3}, 
+          {option: "Yes, but not too badly", value: 2}, 
+          {option: "A little, but it doesn't worry me", value: 1},
+          {option: "Not at all", value: 0}
+        ] // 3, 2, 1, 0
       },
       {
-        
-      }
+        categoryName: "Depression",
+        prompt: "I can laugh and see the funny side of things:",
+        list: [
+          {option: "As much as I always could", value: 0}, 
+          {option: "Not quite so much now", value: 1}, 
+          {option: "Definitely not so much now", value: 2},
+          {option: "Not at all", value: 3}
+        ] // 0 1 2 3
+      },
+      {
+        categoryName: "Anxiety",
+        prompt: "Worrying thoughts go through my mind:",
+        list: [ 
+          {option: "A great deal of the time", value: 3}, 
+          {option: "A lot of the time", value: 2},
+          {option: "From time to time, but not too often", value: 1},
+          {option: "Only occasionally", value: 0}
+        ] // 3, 2, 1, 0
+      },
+      {
+        categoryName: "Depression",
+        prompt: "I feel cheerful: ",
+        list: [ 
+          {option: "Not at all", value: 0}, 
+          {option: "Not often", value: 1}, 
+          {option: "Sometimes", value: 2}, 
+          {option: "Most of the time", value: 3}
+        ] // 0 1 2 3
+      },
+      {
+        categoryName: "Anxiety",
+        prompt: "I can sit at ease and feel relaxed:",
+        list: [ 
+          {option: "Definitely", value: 0}, 
+          {option: "Usually", value: 1},
+          {option: "Not Often", value: 2},
+          {option: "Not at all", value: 3}
+        ] // 3, 2, 1, 0
+      },
+      {
+        categoryName: "Depression",
+        prompt: "I feel as if I am slowed down:",
+        list: [ 
+          {option: "Not at all", value: 3} ,
+          {option: "Not often", value: 2},
+          {option: "Sometimes", value: 1},
+          {option: "Most of the time", value: 0}
+        ] // 3 2 1 0
+      },
+      {
+        categoryName: "Anxiety",
+        prompt: "I get a sort of frightened feeling like 'butterflies' in the stomach:",
+        list: [
+          {option: "Not at all", value: 0},
+          {option: "Occasionally", value: 1},
+          {option: "Quite Often", value: 2},
+          {option: "Very Often", value: 3}
+        ] // 0 1 2 3
+      },
+      {
+        categoryName: "Depression",
+        prompt: "I have lost interest in my appearance:",
+        list: [ 
+          {option: "Definitely", value: 3} ,
+          {option: "I don't take as much care as I should", value: 2},
+          {option: "I may not take quite as much care", value: 1},
+          {option: "I take just as much care as ever", value: 0}
+        ] // 3 2 1 0
+      },
+      {
+        categoryName: "Anxiety",
+        prompt: "I feel restless as I have to be on the  move:",
+        list: [ 
+          {option: "Very much indeed", value: 3} ,
+          {option: "Quite a lot", value: 2},
+          {option: "Not very much", value: 1},
+          {option: "Not at all", value: 0}
+        ] // 3 2 1 0
+      },
+      {
+        categoryName: "Depression",
+        prompt: "I look forward with enjoyment to things:",
+        list: [ 
+          {option: "As much as I ever did", value: 0} ,
+          {option: "Rather less than I used to", value: 1},
+          {option: "Definitely less than I used to", value: 2},
+          {option: "Hardly at all", value: 3}
+        ] // 0 1 2 3
+      },
+      {
+        categoryName: "Anxiety",
+        prompt: "I get sudden feelings of panic:",
+        list: [ 
+          {option: "Very often indeed", value: 3} ,
+          {option: "Quite often", value: 2},
+          {option: "Not very often", value: 1},
+          {option: "Not at all", value: 0}
+        ] // 3 2 1 0
+      },
+      {
+        categoryName: "Depression",
+        prompt: "I can enjoy a good book or radio or TV program:",
+        list: [ 
+          {option: "Often", value: 0} ,
+          {option: "Sometimes", value: 1},
+          {option: "Not often", value: 2},
+          {option: "Very seldom", value: 3}
+        ] // 0 1 2 3
+      },
 
     
     ];
