3f5c471db9c97bdb035e93b82cd2c45f817b11de
Jeditiger05 <john@jedimedia.it>
2020-11-06 04:22:26 +1100

    Modified DisabledMeasurements Endpoint to now Use DayOfYear

--- NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Controllers/PatientController.cs
+++ NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Controllers/PatientController.cs
@@ -118,7 +118,7 @@ namespace NorthernHealthAPI.Controllers
 
                 if (lastRecorded != null)
                 {
-                    if (lastRecorded.DateTimeRecorded.AddDays(pm.Frequency) > DateTime.Now)
+                    if (lastRecorded.DateTimeRecorded.AddDays(pm.Frequency).DayOfYear > DateTime.Now.DayOfYear)
                         disabledMeasurements.Add(pm.MeasurementId);
                 }
 
