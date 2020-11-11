0e37194d5a0b95bd8dbad312ede232db09003645
Steve G <s.grouios@hotmail.com>
2020-11-11 15:49:54 +1100

    Updated DML

--- NHRM-DB/queries.sql
+++ NHRM-DB/queries.sql
@@ -1,14 +1,3 @@
+SELECT * FROM MeasurementRecord
 
-SELECT categoryName,M.measurementName FROM CategoryMeasurement CM
-INNER JOIN Category C ON CM.categoryID = C.categoryID
-INNER JOIN Measurement M ON M.measurementID = CM.measurementID;
-
-SELECT * FROM Measurement
-
-SELECT * FROM MeasurementDataPoint
-
-SELECT * FROM Patient
-
-SELECT * FROM PatientCategory
-
-SELECT * FROM  MeasurementResult
+SELECT * FROM DataPointRecord
