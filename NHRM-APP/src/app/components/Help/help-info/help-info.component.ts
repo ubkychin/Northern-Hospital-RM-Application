54ead060f7e0dd9da6aa2f5f9c5d7efa1051568f
Steve G <s.grouios@hotmail.com>
2020-11-16 10:27:20 +1100

    Added terms/privacy dialogs to Help component

--- NHRM-APP/src/app/components/Help/help-info/help-info.component.ts
+++ NHRM-APP/src/app/components/Help/help-info/help-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ResourceDialog } from 'src/app/models/resource-dialog';
+import { ResourceDialogComponent } from '../../dialogs/resource-dialog/resource-dialog.component';
 
 @Component({
   selector: 'app-help-info',
@@ -7,17 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpInfoComponent implements OnInit {
 
-  constructor() { }
+  dialogConfig: MatDialogConfig;
+  termsDialog: ResourceDialog = {
+    heading: "Terms & Conditions",
+    content: "Awaiting Northern Hospital to supply statement"
+  }
+  privacyDialog: ResourceDialog = {
+    heading: "Privacy Statement",
+    content: "Awaiting Northern Hospital to supply statement"
+  }
+  
+  constructor(public dialog: MatDialog) { 
+    this.dialogConfig = new MatDialogConfig();
+    this.dialogConfig.autoFocus = true;
+  }
 
   ngOnInit(): void {
   }
 
   terms(){
-
+    this.dialogConfig.panelClass = 'information-dialog-container';
+    this.dialogConfig.data = {
+      content: this.termsDialog.content,
+      heading: this.termsDialog.heading
+    }
+    this.dialog.open(ResourceDialogComponent, this.dialogConfig);
   }
 
   privacy(){
-    
+    this.dialogConfig.panelClass = 'information-dialog-container';
+    this.dialogConfig.data = {
+      content: this.privacyDialog.content,
+      heading: this.privacyDialog.heading
+    }
+    this.dialog.open(ResourceDialogComponent, this.dialogConfig);
   }
 
 }
