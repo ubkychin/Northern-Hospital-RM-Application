e27983227265fb24105a50cad83c77dd1d1611b4
Steve G <s.grouios@hotmail.com>
2020-11-15 18:33:19 +1100

    Implemented Alert dialog for Frequency change in IPC component

--- NHRM-APP/src/app/components/ipc/ipc.component.ts
+++ NHRM-APP/src/app/components/ipc/ipc.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { FrequencyChange } from 'src/app/models/frequency-change';
 import { DataService } from 'src/app/services/data.service';
+import { AlertDialogComponent } from '../dialogs/alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'app-ipc',
@@ -9,12 +12,26 @@ import { DataService } from 'src/app/services/data.service';
 export class IpcComponent implements OnInit {
 
   urNumber: string;
+  dialogConfig: MatDialogConfig;
+  duration: string;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, public dialog: MatDialog) {
+    this.dialogConfig = new MatDialogConfig();
+    this.dialogConfig.autoFocus = true;
+    this.dialogConfig.disableClose = false;
     this.urNumber = dataService.patient.value['urNumber'];
     dataService.getFrequencyChange(this.urNumber)
-    .then((res) =>{
+    .then((res: FrequencyChange) =>{
       console.log(res);
+      if(res.change){
+        this.getDuration(res.frequency);
+        this.dialogConfig.panelClass = 'alert-dialog-container';
+            this.dialogConfig.data = {
+              content: 'Your Clinician has updated the duration at which you should be recording your Fluid Drainage, Breath and Pain. It is now expected <strong>' + this.duration + '</strong>',
+              button: false
+            }
+            this.dialog.open(AlertDialogComponent, this.dialogConfig);
+      }
     })
     .catch((err) => console.log(err))
     .finally(() => this.dataService.loading.next(false));
@@ -22,4 +39,18 @@ export class IpcComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  getDuration(frequency) {
+    switch (frequency) {
+      case 1: this.duration = "Daily";
+        break;
+      case 2: this.duration = "Every Other Day";
+        break;
+      case 3: this.duration = "Twice Weekly";
+        break;
+      case 7: this.duration = "Weekly";
+        break;
+      case 28: this.duration = "Monthly"
+    }
+  }
+  
 }
--- NHRM-APP/src/app/models/frequency-change.ts
+++ NHRM-APP/src/app/models/frequency-change.ts
@@ -1,4 +1,4 @@
 export class FrequencyChange {
     change: boolean;
-    frequencySetDate?: Date;
+    frequency?: number;
 }
