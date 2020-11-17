9b00a1ba88c876a521a42435b2475b4f44670500
Jeditiger05 <john@jedimedia.it>
2020-11-17 17:36:07 +1100

    Implemented Async to Login endpoint in AuthController

--- NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Controllers/AuthController.cs
+++ NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Controllers/AuthController.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using NorthernHealthAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace NorthernHealthAPI.Controllers
 {
@@ -24,39 +23,47 @@ namespace NorthernHealthAPI.Controllers
             _context = context;
         }
 
-      //  GET api/auth/patientlogin
-      //  Accepts a Login object - parameters email(string) and password(string). If the model values match a Patient login in the database it
-      //returns a JWT, otherwise Unauthorized result if details invalid, BadRequest if login details improper
-     [HttpPost, Route("patientlogin")]
-        public IActionResult Login(PatientLogin login)
+        [HttpGet]
+        public async Task<IActionResult> Logout()
         {
-           
+            var test = await _context.Patient.Where(p => p.Urnumber.Equals("123456789")).Select(p => new { p.FirstName, p.Dob }).ToListAsync(); ;
+
+            return Ok();
+        }
+
+        //  GET api/auth/patientlogin
+        //  Accepts a Login object - parameters email(string) and password(string). If the model values match a Patient login in the database it
+        //returns a JWT, otherwise Unauthorized result if details invalid, BadRequest if login details improper
+        [HttpPost, Route("patientlogin")]
+        public async Task<IActionResult> Login(PatientLogin login)
+        {
+
             if (login == null)
             {
                 return BadRequest(new { message = "Invalid client request" });
             }
 
-            var patient = _context.Patient.Where(p => p.Email == login.Email).Select(p => new Patient
+            var patient = await _context.Patient.Where(p => p.Email == login.Email).Select(p => new Patient
             {
                 Urnumber = p.Urnumber,
                 Salt = p.Salt,
                 Password = p.Password
-            });
+            }).SingleOrDefaultAsync();
 
-            if(patient.Count() > 0)
+            if (patient != null)
             {
                 var passwordHash = SHA512.Create();
 
-                passwordHash.ComputeHash(Encoding.UTF8.GetBytes(login.Password + patient.SingleOrDefault().Salt + Environment.GetEnvironmentVariable("pepper")));
+                passwordHash.ComputeHash(Encoding.UTF8.GetBytes(login.Password + patient.Salt + Environment.GetEnvironmentVariable("pepper")));
 
-                if (passwordHash.Hash.SequenceEqual(patient.SingleOrDefault().Password))
+                if (passwordHash.Hash.SequenceEqual(patient.Password))
                 {
                     var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("secret")));
                     var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
                     var claims = new[] {
                     new Claim(ClaimTypes.Role, "Patient"),
-                    new Claim("URNumber", patient.SingleOrDefault().Urnumber)
+                    new Claim("URNumber", patient.Urnumber)
                 };
 
                     var tokenOptions = new JwtSecurityToken(
