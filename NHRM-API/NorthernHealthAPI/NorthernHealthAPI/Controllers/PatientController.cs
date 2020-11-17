0ca52512371bbd2e78e14b81ebdde8243715355c
Jeditiger05 <john@jedimedia.it>
2020-11-17 17:08:07 +1100

    Implemented Async to all Endpoins in PAtient controller

--- NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Controllers/PatientController.cs
+++ NHRM-API/NorthernHealthAPI/NorthernHealthAPI/Controllers/PatientController.cs
@@ -30,10 +30,10 @@ namespace NorthernHealthAPI.Controllers
         //  GET api/patient/{urNumber}
         //  Accepts a URNumber (string) and returns a Patient and their categories
         [HttpGet, Route("patient/{urNumber}"), Authorize(Roles = "Patient")]
-        public IActionResult GetPatient(string urNumber)
+        public async Task<IActionResult> GetPatient(string urNumber)
         {
             //Get Patient, Categories and Measurements Assigned
-            var patient = _context.Patient
+            var patient = await _context.Patient
                 .Where(p => p.Urnumber == urNumber)
                 .Select(p => new
                 {
@@ -49,33 +49,33 @@ namespace NorthernHealthAPI.Controllers
                                             .Select(pm => pm.MeasurementId).ToList()
                                         }).ToList()
 
-                }).ToList().SingleOrDefault();
+                }).ToListAsync();
 
-            return Ok(patient);
+            return Ok(patient.SingleOrDefault());
 
         }
 
         //  GET api/frequency/{urNumber}
         //  Accepts a URNumber (string) and returns true if My Drainage Frequncy Changes
         [HttpGet, Route("frequency/{urNumber}"), Authorize(Roles = "Patient")]
-        public IActionResult FrequencyChangeAlert(string urNumber, int categoryId)
+        public async Task<IActionResult> FrequencyChangeAlert(string urNumber, int categoryId)
         {
             var lastFluid = GetLastFluid(urNumber, categoryId);
 
             //return Ok(lastFluid);
             if (lastFluid != null)
             {
-                var frequency = _context.PatientMeasurement
+                var frequency = await _context.PatientMeasurement
                     .Where(pm => pm.Urnumber == urNumber && pm.CategoryId == categoryId && pm.MeasurementId == 4)
                     .Select(pm => new
                     {
                         pm.Frequency,
                         pm.FrequencySetDate
-                    }).ToList().SingleOrDefault();
+                    }).ToListAsync();
 
-                if (lastFluid.DateTimeRecorded < frequency.FrequencySetDate)
+                if (lastFluid.DateTimeRecorded < frequency.SingleOrDefault().FrequencySetDate)
                 {
-                    return Ok(new { change = true, frequency = frequency.Frequency });
+                    return Ok(new { change = true, frequency = frequency.SingleOrDefault().Frequency });
                 }
                 else
                 {
@@ -91,7 +91,7 @@ namespace NorthernHealthAPI.Controllers
         //  GET api/condition
         //  Accepts a URNumber (string) and returns a Patient Condition Details
         [HttpGet, Route("condition/{urNumber}"), Authorize(Roles = "Patient")]
-        public IActionResult GetIPCConditionDetails(string urNumber, int categoryId)
+        public async Task<IActionResult> GetIPCConditionDetails(string urNumber, int categoryId)
         {
             //Get patients last Fluid Drainage Data
             var lastFluid = GetLastFluid(urNumber, categoryId);
@@ -106,7 +106,7 @@ namespace NorthernHealthAPI.Controllers
             if (lastFluid != null && lastBreath != null && lastPain != null)
             {
                 //Get Patient Condition Details for Category
-                var details = _context.ConditionDetails
+                var details = await _context.ConditionDetails
                     .Where(p => p.Urnumber == urNumber && p.CategoryId == categoryId)
                     .Select(p => new
                     {
@@ -122,23 +122,23 @@ namespace NorthernHealthAPI.Controllers
                             BreathScore = lastBreath.Value,
                             PainScore = lastPain.Value
                         }
-                    }).ToList().SingleOrDefault();
+                    }).ToListAsync();
 
-                return Ok(details);
+                return Ok(details.SingleOrDefault());
             }
             else
             {
                 //Get Patient Condition Details for Category without MyDrainage
-                var details = _context.ConditionDetails
+                var details = await _context.ConditionDetails
                     .Where(p => p.Urnumber == urNumber && p.CategoryId == categoryId)
                     .Select(p => new
                     {
                         p.Diagnosis,
                         p.ProcedureDate,
                         p.NextAppointment
-                    }).ToList().SingleOrDefault();
+                    }).ToListAsync();
 
-                return Ok(details);
+                return Ok(details.SingleOrDefault());
             }
 
         }
@@ -146,13 +146,13 @@ namespace NorthernHealthAPI.Controllers
         //  GET api/patient/resources/{urNumber}
         //  Accepts a URNumber (string) and returns all Resources assigned to a Patient 
         [HttpGet, Route("resources/{urNumber}"), Authorize(Roles = "Patient")]
-        public IActionResult GetPatientResources(string urNumber, int categoryId)
+        public async Task<IActionResult> GetPatientResources(string urNumber, int categoryId)
         {
             //Get all Resources that have been assigned to a Patient
-            var resIdList = _context.PatientResource
+            var resIdList = await _context.PatientResource
                 .Where(pr => pr.Urnumber == urNumber && pr.CategoryId == categoryId)
                 .Select(rid => rid.ResourceId)
-                .ToList();
+                .ToListAsync();
 
             if (resIdList.Count() != 0)
             {
@@ -160,16 +160,16 @@ namespace NorthernHealthAPI.Controllers
 
                 foreach (var res in resIdList)
                 {
-                    resourceList.Add(_context.Resource.Where(r => r.ResourceId == res)
+                    resourceList.Add(await _context.Resource.Where(r => r.ResourceId == res)
                          .Select(r => new ResourceCustom
                          {
                              CategoryId = categoryId,
                              Title = r.Title,
                              Prompt = r.Prompt,
-                             ResType = setResource(r.TypeId),
-                             ResContent = setResourceContent(setResource(r.TypeId), r.ResourceId, r.Content)
+                             ResType = GetResource(r.TypeId),
+                             ResContent = GetResourceContent(GetResource(r.TypeId), r.ResourceId, r.Content)
 
-                         }).SingleOrDefault());
+                         }).SingleOrDefaultAsync());
                 }
 
                 return Ok(resourceList);
@@ -181,26 +181,26 @@ namespace NorthernHealthAPI.Controllers
         //  GET api/patient/disabledMeasurements/{urNumber}
         //  Accepts a URNumber (string) and returns all measurements that should be disabled in the app 
         [HttpGet, Route("disabledMeasurements/{urNumber}"), Authorize(Roles = "Patient")]
-        public IActionResult GetMeasurementsToDisable(string urNumber)
+        public async Task<IActionResult> GetMeasurementsToDisable(string urNumber)
         {
             //List of disabled measurements to return
             List<int> disabledMeasurements = new List<int>();
 
             //Get all distinct patient measurements and frequency
-            var patientMeasurements = _context.PatientMeasurement
+            var patientMeasurements = await _context.PatientMeasurement
                 .Where(pm => pm.Urnumber == urNumber)
                 .Select(pm => new
                 {
                     pm.MeasurementId,
                     pm.Frequency
-                }).Distinct().ToList();
+                }).Distinct().ToListAsync();
 
             // Iterate through every measurement and check last record recorded against Frequency 
             foreach (var pm in patientMeasurements)
             {
-                var lastRecorded = _context.MeasurementRecord.
+                var lastRecorded = await _context.MeasurementRecord.
                     Where(mr => mr.MeasurementId == pm.MeasurementId
-                    && mr.Urnumber == urNumber).OrderByDescending(lr => lr.DateTimeRecorded).FirstOrDefault();
+                    && mr.Urnumber == urNumber).OrderByDescending(lr => lr.DateTimeRecorded).FirstOrDefaultAsync();
 
                 if (lastRecorded != null)
                 {
@@ -317,7 +317,7 @@ namespace NorthernHealthAPI.Controllers
         }
 
         // Accepts a typeId (string) as a parameter and returns it's resource type, as a string
-        public static string setResource(int typeId)
+        public static string GetResource(int typeId)
         {
             NHRMDBContext _con = new NHRMDBContext();
 
@@ -326,7 +326,7 @@ namespace NorthernHealthAPI.Controllers
 
         //Accepts a type (string), resId (int) and content (string) as parameters and returns the appropriate content. If the type
         // is a 'dialog' the content returned will be an object with Heading, Content and Video properties
-        private static dynamic setResourceContent(string type, int resId, string content)
+        private static dynamic GetResourceContent(string type, int resId, string content)
         {
             NHRMDBContext _con = new NHRMDBContext();
 
